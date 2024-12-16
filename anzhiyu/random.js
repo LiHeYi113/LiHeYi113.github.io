a221dff538ed319befa6fe211f622cd440ccc2ff
LiHeYi113 <1453502297@qq.com>
2024-12-17 02:18:25 +0800

    Site updated: 2024-12-17 02:18:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/16/lhy/"];function toRandomPost(){
+var posts=["2024/12/16/这是一个擅长离开的世界，而我们都不擅长告别。/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
